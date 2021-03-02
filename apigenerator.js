589fe46f15e03b627f45b9e43b03ec5ac35b8710
rickyreza12 <ricky.reza@trans7.co.id>
2021-03-03 02:34:15 +0700

    apigenerator

---
+++ apigenerator.js
@@ -0,0 +1,71 @@
+[
+  "{{repeat(7)}}",
+  {
+    days: "{{index(0,7)}}",
+    days_name: '{{date(new Date(2014, 0, 1), new Date(), "DD")}}',
+    month: "2",
+    month_name: "February",
+    year: "2021",
+    program: [
+      "{{repeat(10)}}",
+      {
+        title: "{{company()}}",
+        cam: {
+          crew: [
+            "{{repeat(0,7)}}",
+            {
+              nik: "{{integer(100, 999)}}",
+              nama: "{{firstName()}} {{surname()}}",
+              department: '{{random("Produksi", "Art", "TSV")}}',
+              occupation: "Camera Person",
+            },
+          ],
+        },
+        lig: {
+          crew: [
+            "{{repeat(0,7)}}",
+            {
+              nik: "{{integer(100, 999)}}",
+              nama: "{{firstName()}} {{surname()}}",
+              department: '{{random("Produksi", "Art", "TSV")}}',
+              occupation: "Light Person",
+            },
+          ],
+        },
+        aud: {
+          crew: [
+            "{{repeat(0,7)}}",
+            {
+              nik: "{{integer(100, 999)}}",
+              nama: "{{firstName()}} {{surname()}}",
+              department: '{{random("Produksi", "Art", "TSV")}}',
+              occupation: "Audio Person",
+            },
+          ],
+        },
+        crp: {
+          crew: [
+            "{{repeat(0,7)}}",
+            {
+              nik: "{{integer(100, 999)}}",
+              nama: "{{firstName()}} {{surname()}}",
+              department: "Production Services",
+              occupation: "Cripple Person",
+            },
+          ],
+        },
+        tp: {
+          crew: [
+            "{{repeat(0,7)}}",
+            {
+              nik: "{{integer(100, 999)}}",
+              nama: "{{firstName()}} {{surname()}}",
+              department: '{{random("Produksi", "Art", "TSV")}}',
+              occupation: "Town Portal Person",
+            },
+          ],
+        },
+      },
+    ],
+  },
+];

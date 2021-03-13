cd5930e453d1f587bd0dcab7fa0d7abf7fd5ed8d
rickyreza12 <ricky.reza@trans7.co.id>
2021-03-13 16:31:55 +0700

    another one

---
+++ apispec.js
@@ -0,0 +1,45 @@
+[
+  {
+    days: 0,
+    days_name: "Sunday",
+    month: 2,
+    month_name: "February",
+    year: 2021,
+    program: [
+      {
+        title: "Splinx",
+        cam: {
+          occupation: "Camera Person",
+          length: 5,
+          crew: [
+            {
+              nik: 538,
+              nama: "Odonnell Mack",
+              department: "Produksi",
+            },
+            {
+              nik: 222,
+              nama: "Williamson Vaughan",
+              department: "TSV",
+            },
+            {
+              nik: 689,
+              nama: "Elise Moses",
+              department: "Art",
+            },
+            {
+              nik: 772,
+              nama: "Russell Stokes",
+              department: "TSV",
+            },
+            {
+              nik: 329,
+              nama: "Gallegos Barlow",
+              department: "Art",
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
